98927ee9ff774976d2fbeb3850b542bd9e9e2214
Maxim Berezovsky <maksymbe@interlink-ua.com>
2019-07-13 20:32:43 +0300

    Add table with users

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,29 +1,72 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <a-button @click="$router.push('user')">Add</a-button>
+      <a-table bordered :dataSource="users" :columns="columns" rowKey="id">
+        <template slot="operation" slot-scope="text, record">
+          <a-button @click="$router.push({ name: 'user', params: {user: record.id} })">Edit</a-button>
+          <a-button type="danger" @click="$store.commit('REMOVE_USER', record.id)">Delete</a-button>
+        </template>
+      </a-table>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+
+  import Vue from 'vue';
+  import { CREATE_USER, UPDATE_USER } from '../store/user/mutations.type';
+  import { mapGetters } from 'vuex';
+
+  export default Vue.extend({
+    name: 'AppUsers',
+    computed: { ...mapGetters(['users']) },
+    data() {
+      return {
+        columns: [{
+          title: 'Name',
+          dataIndex: 'name',
+          width: '30%',
+          scopedSlots: { customRender: 'name' },
+        }, {
+          title: 'Surname',
+          dataIndex: 'surname',
+        }, {
+          title: 'Email',
+          dataIndex: 'email',
+        },{
+          title: 'Phone',
+          dataIndex: 'phone',
+        }, {
+          title: 'Operation',
+          dataIndex: 'operation',
+          scopedSlots: { customRender: 'operation' },
+        }],
+      }
+    }
+  });
+
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  }
+
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
